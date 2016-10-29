a58fee65ff6d831c9325a7d439c5c0e7ed0746cf
Tyler Wong <tbwong3@gmail.com>
2016-10-29 15:09:07 -0700

    Initial Commit

---
+++ esca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  esca
+//
+//  Created by Tyler Wong on 10/29/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
