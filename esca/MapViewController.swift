87f65a1a2fa40c76ce49cb0f1b298e18e63b1faf
Brandon Vo <bvo124@gmail.com>
2016-12-04 15:00:55 -0800

    Added MapView and CoreLocation

---
+++ esca/MapViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MapViewController.swift
+//  esca
+//
+//  Created by Brandon Vo on 12/4/16.
+//
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import SideMenu
+
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        map.delegate = self
+        map.mapType = .standard
+        map.isZoomEnabled = true
+        map.isScrollEnabled = true
+        map.userTrackingMode = .follow
+        
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last! as CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        map.setRegion(region, animated: true)
+    }
+    
+
+}
