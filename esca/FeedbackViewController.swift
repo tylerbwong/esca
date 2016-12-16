2b5090d5869cf967688a5a2a57aa453331e3a2f1
brittanyberlanga <brittany.berlanga@gmail.com>
2016-12-16 13:41:44 -0800

    Liked add feedback screen to feedback list

--- esca/FeedbackViewController.swift
+++ esca/FeedbackViewController.swift
@@ -48,19 +48,21 @@ class FeedbackViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func addFeedback() {
-        // Go to add feedback
+        self.performSegue(withIdentifier: "addFeedbackFromList", sender: self)
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "addFeedbackFromList" {
+            let vc = segue.destination as! AddFeedbackViewController
+            vc.dealKey = dealKey
+        }
     }
-    */
+ 
     
     // MARK: - Table view data source
     
